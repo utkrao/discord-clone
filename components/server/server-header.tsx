d27b68c9804a8ab4c83c4c22b3d6705fcd751fb1
utkrao <utkarshrao1177@gmail.com>
2023-10-21 16:01:44 +0530

    feat: :sparkles: Delete server: integrated functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -68,7 +68,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           )}
           {isModerator && <DropdownMenuSeparator />}
           {isAdmin && (
-            <DropdownMenuItem className="text-rose-500 dark:text-rose-500 px-3 py-2 text-small cursor-pointer">
+            <DropdownMenuItem onClick={() => onOpen("deleteServer", { server })} className="text-rose-500 dark:text-rose-500 px-3 py-2 text-small cursor-pointer">
               Delete Server
               <Trash className="h-5 w-5 ml-auto" />
             </DropdownMenuItem>
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
 
 interface ModalData {
   server?: Server;
